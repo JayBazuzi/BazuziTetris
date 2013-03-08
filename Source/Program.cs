c9d0ce03a1c4ea74d350d2c8063d14a0badbe595
Jay Bazuzi <JAY@BAZUZI.COM>
2013-03-08 11:34:02 -0800

    Console UI.

--- Source/Program.cs
+++ Source/Program.cs
@@ -11,6 +11,27 @@ namespace BazuziTetris
     {
         static void Main(string[] args)
         {
+            Game game = new Game();
+            do
+            {
+                Console.Clear();
+
+                Console.WriteLine(game);
+
+                var consoleKeyInfo = Console.ReadKey();
+
+                switch (consoleKeyInfo.Key)
+                {
+                    case ConsoleKey.DownArrow:
+                        game.CurrentPiece.DropOneStep();
+                        break;
+
+                    case ConsoleKey.Spacebar:
+                        game.DropAllTheWay();
+                        break;
+                }
+
+            } while (true);
         }
     }
 }
