28147559c34a077ec127e3f37160bf745e92fdd1
Jay Bazuzi <JAY@BAZUZI.COM>
2013-03-08 22:02:09 -0800

    GUI

---
+++ Source/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BazuziTetris
+{
+    public partial class Form1 : Form
+    {
+        Game game = new Game();
+        public Form1()
+        {
+            InitializeComponent();
+            this.timer1.Start();
+        }
+
+        const int blockSize = 20;
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            Brush brushBlank = new SolidBrush(Color.Black);
+            Brush brushBlock = new SolidBrush(Color.Red);
+
+            var gameBitmap = game.Well.Union(game.CurrentPiece.Bitmap, game.CurrentPieceLocation);
+
+            foreach (var x in gameBitmap.HorizontalRange)
+            {
+                foreach (var y in gameBitmap.VerticalRange)
+                {
+                    if (gameBitmap[x, y])
+                    {
+                        g.FillRectangle(brushBlock, x * blockSize, (gameBitmap.Height - y) * blockSize, blockSize, blockSize);
+                    }
+
+                    else
+                    {
+                        g.FillRectangle(brushBlank, x * blockSize, (gameBitmap.Height - y) * blockSize, blockSize, blockSize);
+                    }
+                }
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch(e.KeyCode)
+            {
+                case Keys.Down:
+                    this.game.CurrentPieceDropOneStep();
+                    break;
+
+                case Keys.Up:
+                    this.game.CurrentPieceRotate();
+                    break;
+
+                case Keys.Left:
+                    this.game.MoveLeft();
+                    break;
+
+                case Keys.Right:
+                    this.game.MoveRight();
+                    break;
+
+                case Keys.Space:
+                    this.game.DropAllTheWay();
+                    break;
+            }
+
+            Invalidate();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.game.OnTick();
+            Invalidate();
+        }
+    }
+}
--- Source/Program.cs
+++ Source/Program.cs
@@ -1,66 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Windows.Forms;
 
-namespace BazuziTetris
+static class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Game game = new Game();
-
-            Console.Clear();
-            Console.WriteLine(game);
-
-            var keyThread = new Thread(
-                () =>
-                {
-                    do
-                    {
-                        var consoleKeyInfo = Console.ReadKey();
-
-                        switch (consoleKeyInfo.Key)
-                        {
-                            case ConsoleKey.DownArrow:
-                                game.CurrentPieceDropOneStep();
-                                break;
-
-                            case ConsoleKey.Spacebar:
-                                game.DropAllTheWay();
-                                break;
-
-                            case ConsoleKey.UpArrow:
-                                game.CurrentPieceRotate();
-                                break;
-
-                            case ConsoleKey.LeftArrow:
-                                game.MoveLeft();
-                                break;
-
-                            case ConsoleKey.RightArrow:
-                                game.MoveRight();
-                                break;
-                        }
-                        Console.Clear();
-                        Console.WriteLine(game);
-                    } while (true);
-                }
-            );
-            keyThread.Start();
-
-            do
-            {
-                Thread.Sleep(1000);
-                game.OnTick();
-                Console.Clear();
-                Console.WriteLine(game);
-
-            } while (true);
-        }
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        Application.Run(new BazuziTetris.Form1());
     }
 }
