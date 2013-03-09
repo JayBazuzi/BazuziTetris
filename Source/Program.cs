3b83d3cdc1865438fec3c9109117eb115d0323db
Jay Bazuzi <JAY@BAZUZI.COM>
2013-03-08 21:04:38 -0800

    drop pieces on a timer

--- Source/Program.cs
+++ Source/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace BazuziTetris
@@ -13,36 +14,51 @@ namespace BazuziTetris
         {
             Game game = new Game();
 
+            Console.Clear();
+            Console.WriteLine(game);
+
+            var keyThread = new Thread(
+                () =>
+                {
+                    do
+                    {
+                        var consoleKeyInfo = Console.ReadKey();
+
+                        switch (consoleKeyInfo.Key)
+                        {
+                            case ConsoleKey.DownArrow:
+                                game.CurrentPieceDropOneStep();
+                                break;
+
+                            case ConsoleKey.Spacebar:
+                                game.DropAllTheWay();
+                                break;
+
+                            case ConsoleKey.UpArrow:
+                                game.CurrentPieceRotate();
+                                break;
+
+                            case ConsoleKey.LeftArrow:
+                                game.MoveLeft();
+                                break;
+
+                            case ConsoleKey.RightArrow:
+                                game.MoveRight();
+                                break;
+                        }
+                        Console.Clear();
+                        Console.WriteLine(game);
+                    } while (true);
+                }
+            );
+            keyThread.Start();
+
             do
             {
+                Thread.Sleep(1000);
+                game.OnTick();
                 Console.Clear();
-
                 Console.WriteLine(game);
-
-                var consoleKeyInfo = Console.ReadKey();
-
-                switch (consoleKeyInfo.Key)
-                {
-                    case ConsoleKey.DownArrow:
-                        game.CurrentPieceDropOneStep();
-                        break;
-
-                    case ConsoleKey.Spacebar:
-                        game.DropAllTheWay();
-                        break;
-
-                    case ConsoleKey.UpArrow:
-                        game.CurrentPieceRotate();
-                        break;
-
-                    case ConsoleKey.LeftArrow:
-                        game.MoveLeft();
-                        break;
-
-                    case ConsoleKey.RightArrow:
-                        game.MoveRight();
-                        break;
-                }
 
             } while (true);
         }
